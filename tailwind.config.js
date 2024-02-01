e58fda52f55743efeff8f1c8d7cadb95876ecd0c
Evan Joseph <evandromariano49@gmail.com>
2024-01-31 21:37:53 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'azul-cobalto': '#08072e',
+        'vermelho-vinho': '#a51c30',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
